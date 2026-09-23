0da867097ead9436ac40ddc1ea7b01e5a2945a6d
wioenena <wioenena@gmail.com>
2026-09-23 10:01:46 +0300

    refactor(resources)!: use utils.Optional in emoji
    Change the optional Emoji fields (roles, user, require_colons,
    managed, animated, available) from nullable types to utils.Optional.
    This lets a field that was never sent be told apart from one sent as
    null. id and name stay as ?T because the API always sends them,
    though they can be null.
    
    BREAKING CHANGE: Emoji.roles, user, require_colons, managed, animated
    and available are now utils.Optional(T) and default to .missing
    instead of null.

--- src/resources/emoji/emoji.zig
+++ src/resources/emoji/emoji.zig
@@ -1,3 +1,5 @@
+const utils = @import("zigcord-utils");
+
 const Snowflake = @import("../../snowflake/snowflake.zig").Snowflake;
 const User = @import("../user/user.zig");
 
@@ -5,9 +7,9 @@ const Emoji = @This();
 
 id: ?Snowflake = null,
 name: ?[]const u8 = null,
-roles: ?[]const Snowflake = null,
-user: ?User = null,
-require_colons: ?bool = null,
-managed: ?bool = null,
-animated: ?bool = null,
-available: ?bool = null,
+roles: utils.Optional([]const Snowflake) = .missing,
+user: utils.Optional(User) = .missing,
+require_colons: utils.Optional(bool) = .missing,
+managed: utils.Optional(bool) = .missing,
+animated: utils.Optional(bool) = .missing,
+available: utils.Optional(bool) = .missing,
